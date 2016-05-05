f2892b0c9d537afd0bc59378f63beef9dae351f1
Mohammad Anwari <mdamt@mdamt.net>
2016-05-05 20:57:50 +0700

    Explicitly specify ES7 modules

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 'use strict';
+require("babel-register")({
+  "only" : [ "node_modules/happy-crud/api/*.js", "index.js"]
+});
 
 var base = require('./node_modules/happy-crud/api/base-controller.js');
